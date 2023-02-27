c2649e5d884226a5fdb3ee2045b014f93fa5dd01
PianoMaster-72 <boyuanou@hunterschools.org>
2023-02-27 17:17:27 -0500

    Deleted claw stuff we added to Constants.java We realized they are useless.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,4 @@ public class Constants {
     public final static double LOWER_JOINT_ENCODER_ZERO = 0.44000;
     public final static double LOWER_JOINT_RELATIVE_ENCODER_ZERO = LOWER_JOINT_ENCODER_ZERO * LOWER_JOINT_ENCODER_ZERO;
 
-    public static class Claw {
-        public final static int DOUBLE_SOLENOID_RIGHT_ID = 1;
-        public final static int DOUBLE_SOLENOID_LEFT_ID = 2;
-        public final static int FORWARD_CHANNEL_RIGHT = 3;
-        public final static int REVERRSE_CHANNEL_RIGHT = 4;
-        public final static int FORWARD_CHANNEL_LEFT = 5;
-        public final static int REVERSE_CHANNEL_LEFT = 6;
-    }
-
 }           
